a41425f35515c76d1dd677ce0c5add84f8652adc
Fabiano Carvalho <fbiano15@hotmail.com>
2025-09-23 21:57:34 -0300

    .

--- scripts/busca_ns.js
+++ scripts/busca_ns.js
@@ -39,8 +39,8 @@ async function consultarSerial(serial) {
 
     // Exibe resultado na div
 document.getElementById('resultado').innerHTML = `
-Modelo: ${data.name || 'Não encontrado'} <br>
-Data de Fabricação: ${data.productionDate || 'Não informada'}
+  <p>Modelo: ${data.name || 'Não encontrado'}</p>
+  <p>Data de Fabricação: ${data.data || 'Não encontrado'}</p>
 `;
   } catch (err) {
     document.getElementById('resultado').innerText = `Erro: ${err.message}`;
