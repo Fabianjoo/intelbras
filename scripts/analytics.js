fff7463fa054c49759d35e02a9e3315000d93249
Fabiano Carvalho <fbiano15@hotmail.com>
2025-09-29 20:53:04 -0300

    .

---
+++ scripts/analytics.js
@@ -0,0 +1,5 @@
+// Google Analytics - Script Global
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-0R560VSDN2');
