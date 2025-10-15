a7b0b7f060295a7de9da98c0329d0838a9c37b1b
Fabianjoo <fbiano15@hotmail.com>
2025-10-15 17:15:07 -0300

    Adicionado incontrol nas config

--- scripts/tutoriais/filtros.js
+++ scripts/tutoriais/filtros.js
@@ -36,7 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
             'data-hora', 'ftp', 'rtmp', 'smtp', 'pos', 'espelhamento', 'conta-intelbras', 
             'audio', 'encoder', 'OSD', 'PTZ', 'reconhecimento-facial', 
             'linha-cerca', 'busca-avancada', 'mosaico-visualizacao', 
-            'importar', 'alarme-config', 'notificacoes', 'autoregistro', 'full-color'
+            'importar', 'alarme-config', 'notificacoes', 'autoregistro', 'full-color', 'incontrol'
         ],
         adicionais: ['antigos', 'erros'],
     };
