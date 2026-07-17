ceeb052fe5dc7d3018c5b062e247f9a1424d7c83
Fabianjoo <fbiano15@hotmail.com>
2026-07-17 16:06:12 -0300

    mudado zindex das abas

--- scripts/menu_hamburguer.js
+++ scripts/menu_hamburguer.js
@@ -12,4 +12,13 @@ hamburguer.addEventListener("click", () => {
       hamburguer.classList.remove("ativo");
     }
   });
-  
+
+  document.addEventListener("click", (event) => {
+    const cliqueDentroMenu = paginas.contains(event.target);
+    const cliqueNoHamburguer = hamburguer.contains(event.target);
+
+    if (paginas.classList.contains("ativo") && !cliqueDentroMenu && !cliqueNoHamburguer) {
+      paginas.classList.remove("ativo");
+      hamburguer.classList.remove("ativo");
+    }
+  });
